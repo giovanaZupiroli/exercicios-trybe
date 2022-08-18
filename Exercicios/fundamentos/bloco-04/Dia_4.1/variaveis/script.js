95d566d659700998814448a22f717fd2ada682b2
GiovanaZ <giovanazupiroli@gmail.com>
2022-08-18 15:51:16 -0300

    adiciona exercicios da segunda parte

---
+++ Exercicios/fundamentos/bloco-04/Dia_4.1/variaveis/script.js
@@ -0,0 +1,19 @@
+const myName = "Giovana";
+const birthCity = 'Pederneiras';
+let birthYear = 1997;
+
+console.log (myName);
+console.log (birthCity);
+console.log (birthYear);
+
+birthYear = 2030;
+
+console.log (birthYear);
+
+birthCity = Bauru;
+
+console.log (birthCity);
+
+
+
+
