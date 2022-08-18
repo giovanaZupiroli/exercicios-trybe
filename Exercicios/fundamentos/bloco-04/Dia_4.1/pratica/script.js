af4cb1c6e84b07ba73aa9cede09c5129bb297f98
GiovanaZ <giovanazupiroli@gmail.com>
2022-08-18 19:50:17 -0300

    etapa 2

---
+++ Exercicios/fundamentos/bloco-04/Dia_4.1/pratica/script.js
@@ -0,0 +1,26 @@
+const a = 13;
+const b = 55;
+let resultado;
+// 1
+// Adição
+console.log (a + b);
+// Subtração
+console.log (a - b);
+// Multiplicação
+console.log (a*b);
+// Divisão
+console.log (a/b);
+// Módulo
+console.log (a%b);
+
+// 2
+const c = 3;
+const d = 4;
+let alto2;
+if (c > d) {
+    console.log (alto2 = c)
+} else {
+   console.log (alto2 = d)
+}
+
+// 3
